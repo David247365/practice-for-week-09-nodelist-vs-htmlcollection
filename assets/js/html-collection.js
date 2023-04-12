a499a8ca77b0f60f409d8ea470f1150ab8fb0301
David Soto <ds051128@gmail.com>
2023-04-11 19:02:37 -0500

    initial commit

--- assets/js/html-collection.js
+++ assets/js/html-collection.js
@@ -1,11 +1,11 @@
 export default () => {
-  const bodyChildElements = document.body.children; // HTMLCollection [div]
-  // console.log(bodyChildElements);
+	const bodyChildElements = document.body.children; // HTMLCollection [div]
+	// console.log(bodyChildElements);
 
-  const div = bodyChildElements[0];
+	const div = bodyChildElements[0];
 
-  const divChildElements = div.children; // HTMLCollection [span]
-  const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
-  const span = divChildElements[0]; // <span>Yes!</span>
-  // debugger
+	const divChildElements = div.children; // HTMLCollection [span]
+	const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+	const span = divChildElements[0]; // <span>Yes!</span>
+	// debugger;
 };
